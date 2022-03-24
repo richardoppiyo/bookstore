a4cfbbb1bd8005e2e150a096a7066e20a0e78f4f
Richard Opiyo <romenyo@dewcis.com>
2022-03-24 23:36:37 +0300

    add destructuring to code

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import './Book.css';
+import PropTypes from 'prop-types';
 
-const Book = () => (
-  <>
+const Book = (props) => {
+  const { title, author } = props;
+  return (
     <div className="book">
       <div className="book-holder">
-        <h2>The Book Title</h2>
-        <p>Book Author</p>
+        <h2>
+          {title}
+        </h2>
+        <p>{author}</p>
       </div>
       <button className="book-button" type="button">Delete</button>
     </div>
-  </>
-);
+  );
+};
+
+Book.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+};
 
 export default Book;
